984118d3905d520d96793e8d246343eae3cc76f7
joskic <pepamatousek@email.cz>
2021-09-21 14:38:05 +0200

    Zajda neda klika

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Ahoj ty kokot");
+        System.out.println("Zelena je trava, zajda neda klik");
     }
 }
